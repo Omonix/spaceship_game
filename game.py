f42e6f0927dc140240064ba1fdf82614bd8a58ce
Louis Blonde <louis.blonde24@gmail.com>
2025-01-27 20:59:46 +0100

    add life

--- game.py
+++ game.py
@@ -1,29 +1,36 @@
 import pyxel, random
 
-life = 4
+lifes = 4
 screen_size = (100, 100)
 ship_size = (6, 6)
 ship_pos = (45, 45)
 shoot_size = (1, 4)
+life_list = []
 shoot_list = []
 ennemies_list = []
 boom_list = []
 
 def update():
-    global ship_pos, screen_size, shoot_list, ennemies_list, life
+    global ship_pos, screen_size, shoot_list, ennemies_list, lifes, life_list
     ship_pos = ship_move(ship_pos[0], ship_pos[1])
     shoot_list = new_shoot(ship_pos[0], ship_pos[1], shoot_list)
     shoot_list = shoot_move(shoot_list)
     ennemies_list = new_ennemy(ennemies_list)
     ennemies_list = ennemy_move(ennemies_list)
-    life = ship_dead(life)
+    lifes = ship_dead(lifes)
+    lifes = get_life(lifes)
+    life_list = new_life(life_list)
+    life_list = life_move(life_list)
     ennemy_dead()
+    life_dead()
     boom_anim()
 def draw():
     pyxel.cls(0)
-    if life > 0:
-        pyxel.text(5, 5, f'Life : {life}', 2)
+    if lifes > 0:
+        pyxel.text(5, 5, f'Life : {lifes}', 2)
         pyxel.rect(ship_pos[0], ship_pos[1], ship_size[0], ship_size[1], 1)
+        for life in life_list:
+            pyxel.rect(life[0], life[1], 2, 2, 2)
         for shoot in shoot_list:
             pyxel.rect(shoot[0], shoot[1], 1, 4, 10)
         for ennemy in ennemies_list:
@@ -89,6 +96,30 @@ def boom_anim():
         if boom[2] == 12:
             boom_list.remove(boom)
     return
+def new_life(list):
+    if pyxel.frame_count % 340 == 10:
+        list.append([random.randint(0, 100), 0])
+    return list
+def life_move(list):
+    for life in list:
+        life[1] += 1
+        if life[1] > 100:
+            list.remove(life)
+    return list
+def get_life(lifes):
+    for life in life_list:
+        if abs(ship_pos[0] - life[0]) <= 4 and abs(ship_pos[1] - life[1]) <= 4:
+            life_list.remove(life)
+            lifes += 1
+            new_boom(life[0], life[1])
+    return lifes
+def life_dead():
+    for life in life_list:
+        for shoot in shoot_list:
+            if abs(life[0] - shoot[0]) < 3 and abs(life[1] - shoot[1]) <= 3:
+                life_list.remove(life)
+                shoot_list.remove(shoot)
+                new_boom(life[0], life[1])
 
 pyxel.init(screen_size[0], screen_size[1], title='Game play')
 pyxel.run(update, draw)
